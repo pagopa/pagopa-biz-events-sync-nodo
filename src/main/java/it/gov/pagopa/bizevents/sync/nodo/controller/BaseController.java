8fc236a3f53551ba47489f8e48ea2ab425b36dee
svariant <samuele.varianti@nttdata.com>
2025-03-04 15:56:06 +0100

    feat: [PIDM-265] Implement Info API

---
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/controller/BaseController.java
@@ -0,0 +1,39 @@
+package it.gov.pagopa.bizevents.sync.nodo.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.gov.pagopa.bizevents.sync.nodo.model.AppInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@RestController()
+@Configuration
+@Slf4j
+public class BaseController {
+    @Value("${info.application.name}")
+    private String name;
+
+    @Value("${info.application.version}")
+    private String version;
+
+    @Value("${info.properties.environment}")
+    private String environment;
+
+    @Operation(summary = "health check", description = "Return OK if application is started", security = {@SecurityRequirement(name = "JWT")}, tags = {"Home"})
+    @GetMapping(value = "/info")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<AppInfo> healthCheck() {
+        // Used just for health checking
+        AppInfo info = AppInfo.builder().name(name).version(version).environment(environment).build();
+        return ResponseEntity.status(HttpStatus.OK).body(info);
+    }
+}
