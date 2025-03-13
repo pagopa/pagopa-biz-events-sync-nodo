f30d141d06ee0442cce383fa7519fe08bbe9bcdd
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-13 11:42:34 +0100

    [PIDM-265] fix: including new constants

--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/util/Constants.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/util/Constants.java
@@ -8,10 +8,17 @@ public class Constants {
 
   public static final DateTimeFormatter BIZ_EVENT_DATE_FORMATTER =
       DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+  public static final DateTimeFormatter BIZ_EVENT_EXTENDED_DATE_FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS");
+
+  public static final DateTimeFormatter BIZ_EVENT_REDUCED_DATE_FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
   public static final String ECOMMERCE_HELPDESK_TRANSACTION_SEARCH_ID_TYPE = "PAYMENT_TOKEN";
 
   public static final String HEADER_REQUEST_ID = "X-Request-Id";
 
   public static final String HEADER_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
+
+  public static final Object REGEN_SERVICE_IDENTIFIER = "NDP003PROD_R";
 }
