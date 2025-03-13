800b70edcee4720253691f3f740dc7238617aced
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-13 11:42:03 +0100

    [PIDM-265] feat: including Feign retries and activation annotations

--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/BizEventsSyncNodoApplication.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/BizEventsSyncNodoApplication.java
@@ -2,8 +2,12 @@ package it.gov.pagopa.bizevents.sync.nodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableRetry
 public class BizEventsSyncNodoApplication {
 
   public static void main(String[] args) {
