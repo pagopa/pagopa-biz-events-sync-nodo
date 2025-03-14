2bed111a62d68bbbe750362747201afa04a43916
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-14 13:22:35 +0100

    [PIDM-265] feat: adding new API endpoint for manually trigger sync

--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/controller/BaseController.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/controller/BaseController.java
@@ -3,11 +3,8 @@ package it.gov.pagopa.bizevents.sync.nodo.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import it.gov.pagopa.bizevents.sync.nodo.model.AppInfo;
-import it.gov.pagopa.bizevents.sync.nodo.scheduler.BizEventsSyncNodoScheduler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@Configuration
 @Slf4j
 public class BaseController {
 
@@ -28,8 +24,6 @@ public class BaseController {
   @Value("${info.properties.environment}")
   private String environment;
 
-  @Autowired private BizEventsSyncNodoScheduler scheduler;
-
   @Operation(
       summary = "health check",
       description = "Return OK if application is started",
@@ -39,7 +33,6 @@ public class BaseController {
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<AppInfo> healthCheck() {
 
-    scheduler.synchronizeBizEventsWithNdpReceipts();
     AppInfo info = AppInfo.builder().name(name).version(version).environment(environment).build();
     return ResponseEntity.status(HttpStatus.OK).body(info);
   }
