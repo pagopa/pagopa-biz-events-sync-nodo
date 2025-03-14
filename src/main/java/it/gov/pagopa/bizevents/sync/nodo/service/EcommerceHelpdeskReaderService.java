8145e52b3de602744eef621fdff67bf7724cf728
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-14 10:17:07 +0100

    [PIDM-265] chore: simplified Service handling, removing redundant interfaces

--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/service/EcommerceHelpdeskReaderService.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/service/EcommerceHelpdeskReaderService.java
@@ -1,8 +1,61 @@
 package it.gov.pagopa.bizevents.sync.nodo.service;
 
+import feign.FeignException;
+import it.gov.pagopa.bizevents.sync.nodo.client.EcommerceHelpdeskClient;
 import it.gov.pagopa.bizevents.sync.nodo.entity.bizevents.transaction.TransactionDetails;
+import it.gov.pagopa.bizevents.sync.nodo.model.client.ecommerce.request.SearchTransactionRequest;
+import it.gov.pagopa.bizevents.sync.nodo.model.client.ecommerce.response.SearchTransactionResponse;
+import it.gov.pagopa.bizevents.sync.nodo.model.client.ecommerce.response.TransactionResponse;
+import it.gov.pagopa.bizevents.sync.nodo.model.mapper.BizEventMapper;
+import it.gov.pagopa.bizevents.sync.nodo.util.Constants;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface EcommerceHelpdeskReaderService {
+@Service
+@Slf4j
+public class EcommerceHelpdeskReaderService implements EcommerceHelpdeskReaderService {
 
-  TransactionDetails getTransactionDetails(String paymentToken);
+  private final EcommerceHelpdeskClient ecommerceHelpdeskClient;
+
+  @Autowired
+  public EcommerceHelpdeskReaderService(EcommerceHelpdeskClient ecommerceHelpdeskClient) {
+
+    this.ecommerceHelpdeskClient = ecommerceHelpdeskClient;
+  }
+
+  @Override
+  public TransactionDetails getTransactionDetails(String paymentToken) {
+
+    TransactionDetails transactionDetails = null;
+
+    SearchTransactionRequest request =
+        SearchTransactionRequest.builder()
+            .type(Constants.ECOMMERCE_HELPDESK_TRANSACTION_SEARCH_ID_TYPE)
+            .paymentToken(paymentToken)
+            .build();
+
+    try {
+      SearchTransactionResponse response =
+          this.ecommerceHelpdeskClient.searchTransactionByPaymentToken(0, 10, request);
+
+      if (response.getTransactions() == null || response.getTransactions().isEmpty()) {
+        // TODO throw custom exception
+      }
+
+      List<TransactionResponse> transactions = response.getTransactions();
+      if (transactions.size() > 1) {
+        // TODO log info
+      }
+
+      transactionDetails = BizEventMapper.fromTransactionResponse(transactions.get(0));
+
+    } catch (FeignException e) {
+      // TODO throw custom exception or skip this BizEvent?
+
+    }
+
+    return transactionDetails;
+  }
 }
--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/service/EventHubSenderService.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/service/EventHubSenderService.java
@@ -1,9 +1,77 @@
 package it.gov.pagopa.bizevents.sync.nodo.service;
 
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventDataBatch;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventHubProducerClient;
 import it.gov.pagopa.bizevents.sync.nodo.entity.bizevents.BizEvent;
+import it.gov.pagopa.bizevents.sync.nodo.util.Constants;
+import jakarta.annotation.PreDestroy;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-public interface EventHubSenderService {
+@Service
+@Slf4j
+public class EventHubSenderService implements EventHubSenderService {
 
-  void sendBizEventsToEventHub(List<BizEvent> bizEvents);
+  private final EventHubProducerClient eventHubProducerClient;
+
+  public EventHubSenderService(
+      @Value("${azure.event-hub.connection-string}") String eventHubConnectionString,
+      @Value("${azure.event-hub.producer.name}") String eventHubTopicName) {
+
+    this.eventHubProducerClient =
+        new EventHubClientBuilder()
+            .connectionString(eventHubConnectionString, eventHubTopicName)
+            .buildProducerClient();
+  }
+
+  @PreDestroy
+  private void preDestroy() {
+    this.eventHubProducerClient.close();
+  }
+
+  public void sendBizEventsToEventHub(List<BizEvent> bizEvents) {
+
+    List<EventData> evhEvents =
+        bizEvents.stream()
+            .map(bizEvent -> new EventData(Constants.GSON_PARSER.toJson(bizEvent)))
+            .toList();
+    evhEvents.forEach(
+        eventData ->
+            eventData
+                .getProperties()
+                .put(
+                    Constants.REGEN_SERVICE_IDENTIFIER_KEY,
+                    Constants.REGEN_SERVICE_IDENTIFIER_VALUE));
+
+    //
+    EventDataBatch evhEventBatch = this.eventHubProducerClient.createBatch();
+
+    int batchMaxSize = evhEventBatch.getMaxSizeInBytes();
+    log.debug("Defining batches with maximum dimension of [" + batchMaxSize + "] bytes.");
+
+    for (EventData evhEvent : evhEvents) {
+
+      // Try to add the event from the array to the batch
+      if (!evhEventBatch.tryAdd(evhEvent)) {
+
+        // If the batch is full, send it and then create a new batch
+        this.eventHubProducerClient.send(evhEventBatch);
+        evhEventBatch = this.eventHubProducerClient.createBatch();
+
+        // Try to add that event that couldn't fit before.
+        if (!evhEventBatch.tryAdd(evhEvent)) {
+          log.error("Event is too large for an empty batch. Max size: [" + batchMaxSize + "].");
+          // TODO throw custom exception
+        }
+      }
+    }
+    // send the last batch of remaining events
+    if (evhEventBatch.getCount() > 0) {
+      this.eventHubProducerClient.send(evhEventBatch);
+    }
+  }
 }
