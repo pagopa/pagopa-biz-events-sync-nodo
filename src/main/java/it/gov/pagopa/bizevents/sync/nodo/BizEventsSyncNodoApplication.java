69ad02531d20481b7ad47cc03cffebf136bbcba0
andrea-deri <andrea.derinaldis@pagopa.it>
2025-11-10 17:32:15 +0100

    test

--- src/main/java/it/gov/pagopa/bizevents/sync/nodo/BizEventsSyncNodoApplication.java
+++ src/main/java/it/gov/pagopa/bizevents/sync/nodo/BizEventsSyncNodoApplication.java
@@ -3,10 +3,12 @@ package it.gov.pagopa.bizevents.sync.nodo;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 @EnableFeignClients
 @EnableRetry
 public class BizEventsSyncNodoApplication {
